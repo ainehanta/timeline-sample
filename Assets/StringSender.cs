63bccc03ccf3d049a6f20b370785b9181a6793ea
Makoto Uju <ainehanta@users.noreply.github.com>
2018-11-09 11:47:49 +0900

    quick

---
+++ Assets/StringSender.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using UnityEngine;
+
+public class StringSender : MonoBehaviour
+{
+    public string value = "";
+    public string address = "";
+
+    // Use this for initialization
+    void Start()
+    {
+        sendOsc(this.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Select(string value)
+    {
+        sendOsc(value);
+        this.value = value;
+    }
+
+    protected void sendOsc(string value)
+    {
+        OSCHandler.Instance.SendMessageToClient("Haptic", this.address, value);
+        OSCHandler.Instance.UpdateLogs();
+    }
+}
---
+++ Assets/VelocitySender.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+public class VelocitySender : MonoBehaviour {
+
+    public float velocity = 0;
+    public string address = "";
+
+    protected float prevVelocity;
+
+	// Use this for initialization
+    void Start () {
+        sendOsc(this.velocity);
+        prevVelocity = this.velocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Math.Abs(this.prevVelocity - this.velocity) > 0) {
+            sendOsc(this.velocity);
+            prevVelocity = this.velocity;
+        }
+    }
+
+    protected void sendOsc(float velocity) {
+        OSCHandler.Instance.SendMessageToClient("Haptic", this.address, velocity);
+        OSCHandler.Instance.UpdateLogs();
+    }
+}
